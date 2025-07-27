aaf06f0553baad4ee0dec2c62770bc20df6376b5
mghildiy <manish.c.ghildiyal@gmail.com>
2025-07-27 13:45:35 +0530

    Slice tests for scope controller

--- src/test/java/com/cypherlabs/cypherdigitaltwin/modeling/scope/api/slice/ScopeControllerTest.java
+++ src/test/java/com/cypherlabs/cypherdigitaltwin/modeling/scope/api/slice/ScopeControllerTest.java
@@ -1,6 +1,8 @@
 package com.cypherlabs.cypherdigitaltwin.modeling.scope.api.slice;
 
 import com.cypherlabs.cypherdigitaltwin.modeling.scope.api.dto.CreateScopeRequest;
+import com.cypherlabs.cypherdigitaltwin.modeling.scope.api.exception.ScopeNotFoundException;
+import com.cypherlabs.cypherdigitaltwin.modeling.scope.api.exception.ScopeWithSameNameExistsException;
 import com.cypherlabs.cypherdigitaltwin.modeling.scope.domain.Location;
 import com.cypherlabs.cypherdigitaltwin.modeling.scope.domain.Scope;
 import com.cypherlabs.cypherdigitaltwin.modeling.scope.domain.ScopeStatus;
@@ -26,6 +28,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -59,9 +62,6 @@ public class ScopeControllerTest {
 
     @Test
     void shouldReturnNoScope_whenScopeDoesNotExist() throws Exception {
-        Scope mockScope = new Scope("123", "Scope A",
-                new Location(18.0, 20.0)
-                , Set.of("fake", "dummy"), null);
         when(service.get("123")).thenReturn(Optional.empty());
 
         mvc.perform(get("/scope/123")
@@ -118,7 +118,81 @@ public class ScopeControllerTest {
 
     }
 
-    // .... create with parent
-    // ....when parent scope doesn't exist, expect ErrorResponse
-    // ....when scope with same name, expect ErrorResponse
+    @Test
+    public void shouldCreateScope_withParentScope() throws Exception {
+        String parentId = "122";
+        CreateScopeRequest request = new CreateScopeRequest("Scope A", new Location(18.0, 20.0),
+                Set.of("dummy", "fake"), parentId);
+        Scope parent = new Scope(parentId, "Scope P", new Location(18.0, 20.0),
+                Set.of("dummy", "test"), null);
+        Scope created = new Scope("123", "Scope A", new Location(18.0, 20.0),
+                Set.of("dummy", "fake"), parent
+        );
+
+        when(service.create(
+                eq("Scope A"),
+                eq(new Location(18.0, 20.0)),
+                eq(Set.of("dummy", "fake")),
+                eq(parentId)
+        )).thenReturn(created);
+
+        mvc.perform(post("/scope")
+                        .with(httpBasic("admin", "secret"))
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("123"))
+                .andExpect(jsonPath("$.status").value(ScopeStatus.ACTIVE.name()));
+
+    }
+
+    @Test
+    public void shouldReturnErrorResponse_whenParentScopeNotExists() throws Exception {
+        String parentId = "122";
+        CreateScopeRequest request = new CreateScopeRequest("Scope A", new Location(18.0, 20.0),
+                Set.of("dummy", "fake"), parentId);
+
+        when(service.create(
+                eq("Scope A"),
+                eq(new Location(18.0, 20.0)),
+                eq(Set.of("dummy", "fake")),
+                eq(parentId)
+        )).thenThrow(new ScopeNotFoundException(String.format("Parent scope '%s' not found", parentId)));
+
+        mvc.perform(post("/scope")
+                        .with(httpBasic("admin", "secret"))
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.messages").value(String.format("Parent scope '%s' not found", parentId)))
+                .andExpect(jsonPath("$.errorCode").value("SCOPE_NOT_FOUND"));
+
+    }
+
+    @Test
+    public void shouldReturnErrorResponse_whenScopeWithSameNameExists() throws Exception {
+        String name = "Scope A";
+        CreateScopeRequest request = new CreateScopeRequest(name, new Location(18.0, 20.0),
+                Set.of("dummy", "fake"), null);
+
+        when(service.create(
+                eq(name),
+                eq(new Location(18.0, 20.0)),
+                eq(Set.of("dummy", "fake")),
+                eq(null)
+        )).thenThrow(new ScopeWithSameNameExistsException(String.format("Scope with name '%s' already exists", "Scope A")));
+
+        mvc.perform(post("/scope")
+                        .with(httpBasic("admin", "secret"))
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(jsonPath("$.messages").value(String.format("Scope with name '%s' already exists", name)))
+                .andExpect(jsonPath("$.errorCode").value("SCOPE_WITH_SAME_NAME_EXISTS"));
+
+    }
 }
